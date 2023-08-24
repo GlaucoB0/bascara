3048113de82e05e28cf4b5732fea32414200d06d
Glauco2E <oniiichaan61@gmail.com>
2023-08-23 23:54:41 -0300

    Comentarios adicionados

--- finalizado_dentro2/script_bask.js
+++ finalizado_dentro2/script_bask.js
@@ -1,17 +1,18 @@
 function bask(){
-        
+    
+    //Pegar os valores do input
     let A = document.getElementById("nA").value;
     let B = document.getElementById("nB").value;
     let C = document.getElementById("nC").value;
+
+    //Calcular o delta
     let delta = (B**2)-4 * A * C   
-    console.log(`${A} ${B} ${C}`)
 
-    
-
+    //Formula de bhaskara
     let x1 = ((-B) + (Math.sqrt(delta))) /(2*A)
     let x2 = ((-B) - (Math.sqrt(delta))) /(2*A)
     
-    
+    //Escrever o resultado
     let res = document.getElementById("res")
     res.innerText = `x' = ${x1} \nx" = ${x2} \n△ = ${delta}`
 
